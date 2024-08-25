4edf4da93722f7e040fa0bbfb56c8a881b944768
Omar Alaa <96274721+omaaralaa@users.noreply.github.com>
2024-08-26 02:32:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000,debug=True)
 
 #from flask import Flask, request, jsonify
 #import pickle
